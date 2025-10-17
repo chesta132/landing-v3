bbb7f49c137e5cb89ef3bae7a992ec5cbe03cef8
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-10-17 15:02:51 +0700

    feat: add global types and apply in runtime

---
+++ src/lib/extends.ts
@@ -0,0 +1,84 @@
+import { NODE_ENV } from "@/config";
+
+JSON.isJSON = function (text) {
+  try {
+    JSON.parse(text);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+JSON.safeParse = function <T>(text?: string | Falsy, fallback?: T) {
+  try {
+    return this.parse(text || "invalid");
+  } catch {
+    return fallback;
+  }
+};
+
+Object.compare = function <T extends object>(...objectsProp: T[]) {
+  const objects = [...objectsProp];
+  let longestObjectKeys = 0;
+  let longestObjectKeysIndex = 0;
+
+  objects.forEach((object, index) => {
+    const length = Object.entries(object).length;
+    if (longestObjectKeys < length) {
+      longestObjectKeys = length;
+      longestObjectKeysIndex = index;
+    }
+  });
+  const [longestObject] = objects.splice(longestObjectKeysIndex, 1);
+
+  return Object.entries(longestObject).every(([key, value]) => {
+    return objects.every((object) => Object.getOwnPropertyNames(object).includes(key) && object[key as keyof T] === value);
+  });
+};
+
+console.debug = function (message?: "NO_TRACE" | (string & {}), ...data: any[]) {
+  if (NODE_ENV === "production") return;
+  const noTrace = message === "NO_TRACE";
+
+  if (noTrace) {
+    this.groupCollapsed(...data);
+  } else {
+    this.groupCollapsed(message, ...data);
+  }
+
+  if (!noTrace) {
+    this.trace();
+  }
+
+  console.groupEnd();
+};
+
+console.debugTable = function (tabularData, properties, trace) {
+  if (NODE_ENV === "production") return;
+  const createdError = new Error();
+  const callerLine = createdError.stack?.split("\n")[2].trim().slice(2);
+  const noTrace = trace === "NO_TRACE";
+  const superTrace = trace === "SUPER_TRACE";
+
+  if (!noTrace) this.log("Called by: " + callerLine);
+  else if (superTrace) this.trace();
+
+  this.table(tabularData, properties);
+};
+
+Object.isObject = function <T>(object: T) {
+  if (typeof object === "object" && object !== null && !(object instanceof Date)) return true;
+  return false;
+};
+
+Object.typedEntries = function <T extends object>(object: T) {
+  return this.entries(object) as [keyof T, T[keyof T]][];
+};
+
+Object.typedKeys = function <T extends object>(object: T) {
+  return this.keys(object) as (keyof T)[];
+};
+
+Object.typedValues = function <T extends object>(object: T) {
+  return this.values(object) as T[keyof T][];
+};
---
+++ src/types/global.d.ts
@@ -0,0 +1,181 @@
+interface Console {
+  /**
+   * Console log when its not in production env
+   */
+  debug(message?: "NO_TRACE" | (string & {}), ...data: any[]): void;
+
+  /**
+   * Console table when its not in production env
+   */
+  debugTable(tabularData: any, properties?: readonly string[], trace?: "NO_TRACE" | "SUPER_TRACE"): void;
+}
+
+interface JSON {
+  /**
+   * Checks if a given value is a valid JSON string.
+   *
+   * @param text - The value to test
+   * @returns True if the value is a valid JSON string, false otherwise
+   *
+   * @example
+   * JSON.isJSON('{"a":1}') // true
+   * JSON.isJSON('invalid') // false
+   */
+  isJSON: (text: string) => boolean;
+
+  /**
+   * Safely parses a JSON string into a JavaScript value.
+   * Returns the parsed value if valid, or the given fallback value if parsing fails.
+   *
+   * @param text - The JSON string to parse.
+   * @param fallback - The value to return if parsing fails (default: undefined).
+   * @returns The parsed value if valid JSON, otherwise the fallback.
+   *
+   * @example
+   * JSON.safeParse('{"a":1}') // { a: 1 }
+   * JSON.safeParse<number>("123", 0) // 123
+   * JSON.safeParse<number>("not-a-number", 0) // 0
+   */
+  safeParse: <T = any>(text: string | Falsy, fallback?: T, options?: { mergeWithFallback?: boolean }) => T;
+}
+
+interface ObjectConstructor {
+  /**
+   * Compares multiple objects to see if all share the same key-value pairs
+   * for the keys present in the "largest" object (the one with most keys).
+   *
+   * @param objects - A list of objects to compare
+   * @returns True if all objects have matching values for all keys of the largest object, false otherwise
+   *
+   * @example
+   * Object.compare({a:1,b:2},{a:1,b:2}) // true
+   * Object.compare({a:1,b:2},{a:1,b:3}) // false
+   */
+  compare<T extends object>(...objects: T[]): boolean;
+
+  /**
+   * Check prop is it object or not.
+   *
+   * @param object - Original object.
+   *
+   * @returns Boolean of prop is object or not
+   */
+  isObject<T extends object>(object: T): boolean;
+
+  /**
+   * Object.typedEntries dengan typing yang lebih strict.
+   *
+   * @example
+   * ```ts
+   * const object = { foo: 1, bar: "yo" };
+   * for (const [k, v] of Object.typedEntries(object)) {
+   *   // k: "foo" | "bar"
+   *   // v: number | string
+   * }
+   * ```
+   */
+  typedEntries<T extends object>(object: T): [keyof T, T[keyof T]][];
+
+  /**
+   * Returns typed keys of the given object with stricter typing.
+   *
+   * @param object - Original object
+   * @returns Array of keys with strict typing
+   *
+   * @example
+   * ```ts
+   * const object = { foo: 1, bar: "yo" };
+   * const keys = Object.typedKeys(object);
+   * // keys: ("foo" | "bar")[]
+   * ```
+   */
+  typedKeys<T extends object>(object: T): (keyof T)[];
+
+  /**
+   * Returns typed values of the given object with stricter typing.
+   *
+   * @param object - Original object
+   * @returns Array of values with strict typing
+   *
+   * @example
+   * ```ts
+   * const object = { foo: 1, bar: "yo" };
+   * const values = Object.typedValues(object);
+   * // values: (number | string)[]
+   * ```
+   */
+  typedValues<T extends object>(object: T): T[keyof T][];
+}
+
+/** Union type representing all base JS types. */
+type AllType = Function | string | number | boolean | object | symbol | bigint;
+
+/** Utility type that removes all fields from `T` whose value type extend `U`. */
+type OmitByValue<T, U> = {
+  [K in keyof T as U extends T[K] ? never : K]: T[K];
+};
+
+/** Utility type that pick all fields from `T` whose value type extend `U`. */
+type PickByValue<T, U> = {
+  [K in keyof T as U extends T[K] ? K : never]: T[K];
+};
+
+/** Strict version OmitByValue. */
+type OmitByValueStrict<T, U> = {
+  [K in keyof T as T[K] extends U ? never : K]: T[K];
+};
+
+/** Strict version PickByValue. */
+type PickByValueStrict<T, U> = {
+  [K in keyof T as T[K] extends U ? K : never]: T[K];
+};
+
+/** Type representing all falsy values. */
+type Falsy = "" | 0 | false | null | undefined;
+
+/** Type representing all truthy values. */
+type Truthy = Exclude<AllType, Falsy>;
+
+/** Returns `TrueType` if `T` is not falsy, otherwise `FalseType`. */
+type IsTruthy<T, TrueType = T, FalseType = never> = [T] extends [Falsy] ? FalseType : TrueType;
+
+/** Returns `TrueType` if `T` is falsy, otherwise `FalseType`. */
+type IsFalsy<T, TrueType = T, FalseType = never> = [T] extends [Falsy] ? TrueType : FalseType;
+
+/** Returns `TrueType` if `T` is array, otherwise `FalseType`. */
+type IsArray<T, TrueType = T, FalseType = never> = [T] extends [any[]] ? TrueType : FalseType;
+
+/** IsArray but not strict. */
+type IncludeArray<T, TrueType = T, FalseType = never> = T extends any[] ? TrueType : FalseType;
+
+/** Extracts the element type of an array `T`. */
+type ExtractArray<T> = T extends (infer U)[] ? U : T;
+
+/** Conditionally adds a new field to a type `T`. */
+type ConditionalField<T, Key extends string, ExtraKey extends string, ExtraType> = IsFalsy<T[Key], T, T & { [K in ExtraKey]: ExtraType }>;
+
+/** Replaces all occurrences of substring `W` in string `S` with `R`. */
+type Replace<S extends string, F extends string, R extends string> = S extends `${infer First}${F}${infer Last}` ? `${First}${R}${Last}` : S;
+
+/** Allows only one key of T to exist at a time. */
+type OneFieldOnly<T extends Record<string, unknown>> = {
+  [K in keyof T]: {
+    [P in K]: T[P];
+  } & {
+    [P in Exclude<keyof T, K>]?: never;
+  };
+}[keyof T];
+
+/** Requires at least one key from Keys to exist in T. */
+type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
+  }[Keys];
+
+/** Ensures either Keys or Others exist, but not both. */
+type EitherWithKeys<Keys extends object, Others extends object> =
+  | (Keys & { [K in keyof Others]?: undefined })
+  | (Others & { [K in keyof Keys]?: never });
+
+/** Flattens a union type U into a single type. */
+type MergeUnion<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
