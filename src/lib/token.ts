555c8b8043bcf5591c09d6da6cc9283951306f9a
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-10-17 15:05:26 +0700

    feat: token lib

---
+++ src/lib/token.ts
@@ -0,0 +1,50 @@
+import jwt from "jsonwebtoken";
+import { NODE_ENV } from "@/config";
+import { timeInMs } from "./manipulate/number";
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
+
+export const accessTokenConfig: Omit<ResponseCookie, "name" | "value"> = {
+  httpOnly: true,
+  secure: NODE_ENV === "production",
+  sameSite: "strict",
+  maxAge: timeInMs({ minute: 5 }),
+};
+
+export const refreshTokenConfig: Omit<ResponseCookie, "name" | "value"> = {
+  httpOnly: true,
+  secure: NODE_ENV === "production",
+  sameSite: "strict",
+  maxAge: timeInMs({ week: 2 }),
+};
+
+export const refreshTokenSessionOnlyConfig: Omit<ResponseCookie, "name" | "value"> = {
+  httpOnly: true,
+  secure: NODE_ENV === "production",
+  sameSite: "strict",
+};
+
+type ExpiresIn = Parameters<typeof jwt.sign>[2]["expiresIn"];
+
+export const createAccessToken = (payload: jwt.JwtPayload, expiresIn?: ExpiresIn | null) => {
+  return jwt.sign(payload, process.env.JWT_ACCESS_SECRET!, expiresIn !== null ? { expiresIn: "5m" } : {});
+};
+
+export const createRefreshToken = (payload: jwt.JwtPayload, expiresIn?: ExpiresIn | null) => {
+  return jwt.sign(payload, process.env.JWT_REFRESH_SECRET!, expiresIn !== null ? { expiresIn: "2w" } : {});
+};
+
+export const verifyAccessToken = (token: string) => {
+  try {
+    return jwt.verify(token, process.env.JWT_ACCESS_SECRET!) as jwt.JwtPayload;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const verifyRefreshToken = (token: string) => {
+  try {
+    return jwt.verify(token, process.env.JWT_REFRESH_SECRET!) as jwt.JwtPayload;
+  } catch (error) {
+    return null;
+  }
+};
