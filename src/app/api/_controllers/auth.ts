75447c0e86cd186d9cd46e8d9c9b6e511215d320
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-10-19 13:42:11 +0700

    feat: add 2fa in sign in route

--- src/app/api/_controllers/auth.ts
+++ src/app/api/_controllers/auth.ts
@@ -1,5 +1,7 @@
+import { timeInMs } from "@/lib/manipulate/number";
 import prisma from "@/services/db/client";
 import crud from "@/services/db/crud";
+import { generateOTP, sendAuthConfirmationEmail, sendOTPEmail } from "@/services/email/mailer";
 import { ServerError } from "@/services/server-error";
 import { ApiRequest, ApiResponse } from "@/types/server";
 import { Admin } from "@prisma/client";
@@ -7,12 +9,13 @@ import bcrypt from "bcrypt";
 
 export type SigninPayload = Pick<Admin, "email" | "password"> & { rememberMe: boolean };
 export type SignupPayload = Pick<Admin, "email" | "password" | "name"> & { rememberMe: boolean };
+export type SigninResponse = { type: "CONFIRMATION" | "OTP"; session: string };
 
 export abstract class AuthController {
   static neededBodySignin = ["email", "password", "rememberMe"];
   static neededBodySignup = ["email", "password", "rememberMe", "name"];
 
-  static async signin(req: ApiRequest<SigninPayload, never, never>, { reply }: ApiResponse<Admin>) {
+  static async signin(req: ApiRequest<SigninPayload, never, never>, { reply }: ApiResponse<SigninResponse>) {
     const { email, password, rememberMe } = req.body;
 
     const admin = await crud.getOne(prisma.admin, { email }, { error: null });
@@ -25,7 +28,31 @@ export abstract class AuthController {
       throw new ServerError("CLIENT_FIELD", { field: "password", message: "Incorrect Password" });
     }
 
-    reply.success(admin).setCookie({ template: "REFRESH_ACCESS", rememberMe }).respond();
+    const otp = generateOTP();
+    const session = crypto.randomUUID();
+    const expires = new Date(Date.now() + timeInMs({ minute: 10 }));
+    const secret = `otp=${otp}_session=${session}_rememberMe=${String(rememberMe)}`;
+
+    switch (admin.auth) {
+      case "CONFIRMATION":
+        await crud.createOne(prisma.verification, {
+          expires,
+          secret,
+          type: "CONFIRMATION_AUTH",
+        });
+        await sendAuthConfirmationEmail(admin.email, secret, admin.name);
+        reply.success({ type: "CONFIRMATION", session }).respond();
+        break;
+      case "OTP":
+        await crud.createOne(prisma.verification, {
+          expires,
+          secret,
+          type: "OTP_AUTH",
+        });
+        await sendOTPEmail(admin.email, otp, admin.name);
+        reply.success({ type: "OTP", session }).respond();
+        break;
+    }
   }
 
   static async signup(req: ApiRequest<SignupPayload, never, never>, { reply }: ApiResponse<Admin>) {
