bff1fece68ac3f948dad9b39543d1c4dcae4a9d5
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-10-20 18:35:21 +0700

    refactor: pluralize profile endpoint and change endpoint format to root level path

--- src/app/api/auth/ep.d.ts
+++ src/app/api/auth/ep.d.ts
@@ -4,11 +4,11 @@ declare module "@/types/endpoints" {
   namespace Endpoints {
     interface Auth {
       post:
-        | BuildEndpoint<"/auth/sign-up", typeof AuthController.signup>
-        | BuildEndpoint<"/auth/sign-in", typeof AuthController.signin>
-        | BuildEndpoint<"/auth/sign-in/otp", typeof AuthController.signinByOtp>
-        | BuildEndpoint<"/auth/sign-in/confirm", typeof AuthController.confirmSignin>;
-      get: BuildEndpoint<"/auth/sign-in/confirm", typeof AuthController.signinByConfirm>;
+        | BuildEndpoint<"/sign-up", typeof AuthController.signup>
+        | BuildEndpoint<"/sign-in", typeof AuthController.signin>
+        | BuildEndpoint<"/sign-in/otp", typeof AuthController.signinByOtp>
+        | BuildEndpoint<"/sign-in/confirm", typeof AuthController.confirmSignin>;
+      get: BuildEndpoint<"/sign-in/confirm", typeof AuthController.signinByConfirm>;
     }
   }
   interface Endpoints extends Endpoints.Auth {}
---
+++ src/app/api/profiles/ep.d.ts
@@ -0,0 +1,13 @@
+import { ProfileController } from "../_controllers/profile";
+
+declare module "@/types/endpoints" {
+  namespace Endpoints {
+    interface Profile {
+      get: BuildEndpoint<"/", typeof ProfileController.get>;
+      post: BuildEndpoint<"/", typeof ProfileController.create>;
+      put: BuildEndpoint<"/{id}", typeof ProfileController.update>;
+      delete: BuildEndpoint<"/{id}", typeof ProfileController.delete>;
+    }
+  }
+  interface Endpoints extends Endpoints.Profile {}
+}
