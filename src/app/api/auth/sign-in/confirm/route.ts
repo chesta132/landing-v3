31c5cc7581631846135a4bf89a55f225ad82ea64
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-10-20 15:43:23 +0700

    refactor: update authentication confirmation route to use GET method

--- src/app/api/auth/sign-in/confirm/route.ts
+++ src/app/api/auth/sign-in/confirm/route.ts
@@ -1,4 +1,4 @@
 import { AuthController } from "@/app/api/_controllers/auth";
 import { Route } from "@/lib/server/route";
 
-export default Route.create({ POST: AuthController.signinByConfirm });
+export default Route.create({ POST: AuthController.confirmSignin, GET: AuthController.signinByConfirm });
--- src/app/api/auth/sign-in/confirmed/route.ts
+++
@@ -1,4 +0,0 @@
-import { AuthController } from "@/app/api/_controllers/auth";
-import { Route } from "@/lib/server/route";
-
-export default Route.create({ POST: AuthController.signinByConfirm });
